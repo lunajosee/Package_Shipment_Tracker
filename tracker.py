13f0e083f77ac7da730389326feacd2ef595eef1
Jose Luna <lunajose1111@gmail.com>
2026-09-11 00:40:32 -0400

    Add basic menu display and input prompt

---
+++ tracker.py
@@ -0,0 +1,21 @@
+"""
+Program Name: Package Shipment Tracker
+Author: Jose Luna
+Purpose: This program tracks the shipment of packages and provides updates on their status.
+Resources: None
+Date: 2026-09-11
+"""
+
+def main():
+    """Run the main menu loop for the package tracker program."""
+    print("Package Shipment Tracker")
+    print("1. Add package")
+    print("2. Update package status")
+    print("3. Search for package")
+    print("4. View all packages")
+    print("5. Quit")
+
+    choice = input("Enter your choice: ")
+    print("You entered:", choice)
+
+main()

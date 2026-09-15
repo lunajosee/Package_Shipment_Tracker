195fe54a8146420111accafa6dd867446e013b44
Jose Luna <lunajose1111@gmail.com>
2026-09-14 22:57:22 -0400

    Add while loop so menu repeats until user quits

--- tracker.py
+++ tracker.py
@@ -8,14 +8,26 @@ Date: 2026-09-11
 
 def main():
     """Run the main menu loop for the package tracker program."""
-    print("Package Shipment Tracker")
-    print("1. Add package")
-    print("2. Update package status")
-    print("3. Search for package")
-    print("4. View all packages")
-    print("5. Quit")
+    running = True
+
+    while running:
+        print("Package Shipment Tracker")
+        print("1. Add package")
+        print("2. Update package status")
+        print("3. Search for package")
+        print("4. View all packages")
+        print("5. Quit")
+
+        choice = input("Enter your choice: ")
+        print("You entered:", choice)
+
+        if choice == "5":
+            running = False
 
     choice = input("Enter your choice: ")
     print("You entered:", choice)
+
+    if choice == input("Enter your choice: "):
+        print("You entered:", choice)
 
 main()
